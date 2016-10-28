0942f758db00d4300d435efda8ed55a90f8814f6
Subha Arulselvam <sarulse@users.noreply.github.com>
2016-10-28 11:27:23 -0400

    Update urlParams.php

--- SearchPersonRecords/urlParams.php
+++ SearchPersonRecords/urlParams.php
@@ -6,7 +6,6 @@ $domain_name2 = "www.xxxx.com/phptest.php";
 $username = "test";
 $password = "test104";
 $urlParams1 = 'http://'.$domain_name1.'?'.'username='.$username.'&'.'password='.$password;
-
 $urlParams2 = 'http://'.$domain_name2.'?'.'username='.$username.'&'.'password='.$password;
 
 ?>
