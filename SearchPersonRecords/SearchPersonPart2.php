47d70c7760e7d1116fbaca7105db4ea4449e2e8d
Subha Arulselvam <sarulse@users.noreply.github.com>
2016-11-02 14:25:02 -0400

    Update SearchPersonPart2.php

--- SearchPersonRecords/SearchPersonPart2.php
+++ SearchPersonRecords/SearchPersonPart2.php
@@ -12,18 +12,7 @@
 <head>
 <meta charset="utf-8"/>
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
-<script>
-	function validatePhone(){  
-            var num = document.forms["personSearch"]["phone"].value;    
-            var pattern = /^\(?(\d{3})\)?[-. ]?(\d{3})[-. ]?(\d{4})$/;  
-            var result = pattern.test(num); 
-            var mesg = "Enter a valid US Phone Number Format:\n(123)-4567890\n (or) 1234567890\n (or) 123-456-7890\n (or) 123-4567890";
-            if (result == false) {
-                alert(mesg);
-                return false;                   
-            }              
-    	} 
-</script>
+<script src = "validatePhone.js"></script>
 <link rel="stylesheet" type="text/css" href="formstyle.css">
 </head>
 <body>
