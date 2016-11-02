f8bf203a9dc92642ed68949cef82acb3895fd5d8
Subha Arulselvam <sarulse@users.noreply.github.com>
2016-11-02 11:57:52 -0400

    Update displayDetail.php

--- SearchPersonRecords/displayDetail.php
+++ SearchPersonRecords/displayDetail.php
@@ -26,12 +26,10 @@
 		$name = array();
 		$fname = $_GET['firstname'];
 		$lname =$_GET['lastname'];
-		$state= $_GET['state'];
-		
+		$state= $_GET['state'];		
 		$name['fname']= $fname;
 		$name['lname']= $lname;
-		$name['state']= $state;
-		
+		$name['state']= $state;		
 		return $name;			
 	}
 	//Create another xml request
@@ -86,38 +84,38 @@
 		}
 		else
 		{
-		    		//$count counts the no. of records in the xml file
-				$count = count($xml_data->recordset->record);				
-				if ($count>0)
-				{
-					//echo "Number of Records in the address book: ".$count."</br>";
-					$match_found = 0;
-					$resultArray =array();
-					for ($i=0; $i<$count; $i++)
-					{                   
-							
-							$firstname = $xml_data->recordset->record[$i]->firstname;
-							$lastname = $xml_data->recordset->record[$i]->lastname;
-							$stateName = $xml_data->recordset->record[$i]->state;
-							$cityName = $xml_data->recordset->record[$i]->city;                
-							$fNameCheck =strcasecmp($fname,$firstname);
-							$lNameCheck= strcasecmp($lname,$lastname);
-							$stateCheck= strcasecmp($state,$stateName);
-							if(($fNameCheck ==0) && ($lNameCheck == 0)&& ($stateCheck == 0))
-							{								
-								$match_found++;
-								//print_r($xml_data->recordset->record[$i]);
-								$recordset = $xml_data->recordset;
-								$match_record = $recordset->record[$i];
-								array_push($resultArray,$match_record);
-							}                                                       
-									  
-					} 
-					//call printRecord function() to print Results
-					printRecord($resultArray);
-				}	
-			}		
-		
+			//$count counts the no. of records in the xml file
+			$count = count($xml_data->recordset->record);				
+			if ($count>0)
+			{
+				//echo "Number of Records in the address book: ".$count."</br>";
+				$match_found = 0;
+				$resultArray =array();
+				for ($i=0; $i<$count; $i++)
+				{                   
+
+						$firstname = $xml_data->recordset->record[$i]->firstname;
+						$lastname = $xml_data->recordset->record[$i]->lastname;
+						$stateName = $xml_data->recordset->record[$i]->state;
+						$cityName = $xml_data->recordset->record[$i]->city;                
+						$fNameCheck =strcasecmp($fname,$firstname);
+						$lNameCheck= strcasecmp($lname,$lastname);
+						$stateCheck= strcasecmp($state,$stateName);
+						if(($fNameCheck ==0) && ($lNameCheck == 0)&& ($stateCheck == 0))
+						{								
+							$match_found++;
+							//print_r($xml_data->recordset->record[$i]);
+							$recordset = $xml_data->recordset;
+							$match_record = $recordset->record[$i];
+							array_push($resultArray,$match_record);
+						}                                                       
+
+				} 
+				//call printRecord function() to print Results
+				printRecord($resultArray);
+			}	
+		}		
+
 	}
 	//print Records
    	function printRecord($outputArray){		
@@ -125,15 +123,15 @@
 		//echo "Number of Matches: ".$num_match;
 		if ($num_match == 0)
 		{
-					exit( "\nNo person can be found with the given details\n");            
+			exit( "\nNo person can be found with the given details\n");            
 		}
 		else if ($num_match == 1)
 		{
-				echo "Only one match found"."<br/>";
-				for ($counter=0; $counter<$num_match; $counter++)
-				{
-					displayDetail($outputArray,$counter);
-				}
+			echo "Only one match found"."<br/>";
+			for ($counter=0; $counter<$num_match; $counter++)
+			{
+				displayDetail($outputArray,$counter);
+			}
 		}
 		else
 		{
@@ -142,7 +140,6 @@
 			for ($counter=0; $counter<$num_match; $counter++)
 			{
 				?>
-		
 				<div class="results">
 				<div id="showResult<?php echo $counter?>">
 				<a href="#showResult">
@@ -152,8 +149,7 @@
 					</a>
 					<a id="showResult"></a>
 					</div> 
-					<div class="showDetail<?php echo $counter?>">               
-				
+					<div class="showDetail<?php echo $counter?>">            
 				<?php              
 					displayDetail($outputArray,$counter);							 
 				 ?>
